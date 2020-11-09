d30206359c468a0c63e611888b7c8c8b700e09fc
曹磊 <lcaotx@gmail.com>
2020-11-09 15:21:07 +0800

    初始化，并增加单例模式

---
+++ tests/Feature/SingletonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\DesignPatterns\Creational\Singleton\Singleton;
+use Tests\TestCase;
+
+class SingletonTest extends TestCase
+{
+    public function testUniqueness()
+    {
+        $firstCall = Singleton::getInstance();
+        $secondCall = Singleton::getInstance();
+
+        $this->assertInstanceOf(Singleton::class, $firstCall);
+        $this->assertSame($firstCall, $secondCall);
+    }
+}
